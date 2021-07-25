d3bd3cf0c42e084f495312e263350c69c51ced06
Celio Iyama <celio.iyama@gmail.com>
2021-07-25 13:37:12 -0300

    Service layer, component registration

---
+++ request/src/main/java/com/ciyama/request/services/UserService.java
@@ -0,0 +1,26 @@
+package com.ciyama.request.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ciyama.request.entities.User;
+import com.ciyama.request.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User>  obj = repository.findById(id);
+		return obj.get();
+	}
+}
