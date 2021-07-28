7b01b659126862f6195e33a4f1fa04337ef9c1b9
Celio Iyama <celio.iyama@gmail.com>
2021-07-27 21:24:09 -0300

    Exception handling - findById

--- request/src/main/java/com/ciyama/request/services/UserService.java
+++ request/src/main/java/com/ciyama/request/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.ciyama.request.entities.User;
 import com.ciyama.request.repositories.UserRepository;
+import com.ciyama.request.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User>  obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
